3881569ec5dccb0fcda7c2b16dc2c009722060b0
samisaafi <127246541+samisaafi@users.noreply.github.com>
2023-05-19 18:55:22 +0000

    Update FullName.js

--- src/components/FullName.js
+++ src/components/FullName.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import './FullName.css';
+// Define the FullName component
 
 function FullName() {
+// Return the JSX representing the full name
   return (
     <h1>Saafi sami</h1>
   );
