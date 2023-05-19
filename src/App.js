bb7a1836662ce80868ed2847c1d840e5bc6e052e
samisaafi <127246541+samisaafi@users.noreply.github.com>
2023-05-19 18:56:45 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,10 @@ import React from 'react';
 import ProfilePhoto from './components/ProfilePhoto';
 import FullName from './components/FullName';
 import Address from './components/Adress';
+// Define the App component
+
 function App() {
+// Return the JSX representing the structure of the app
   return (
     <div className="App">
       <ProfilePhoto />
