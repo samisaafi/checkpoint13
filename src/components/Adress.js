0265b390975b652e4dd2ecc087862e1794c5a9c0
samisaafi <127246541+samisaafi@users.noreply.github.com>
2023-05-19 18:54:20 +0000

    Update Adress.js

--- src/components/Adress.js
+++ src/components/Adress.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import './Adress.css';
-
+// Define the Address component
 function Adress() {
+   // Return the JSX representing the address
+
   return (
     <p>rue du cair<br />nouvelle medina 3</p>
   );
