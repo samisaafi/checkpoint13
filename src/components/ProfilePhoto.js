555379fc858127c38dee131565138ee591b5478c
samisaafi <127246541+samisaafi@users.noreply.github.com>
2023-05-19 18:55:53 +0000

    Update ProfilePhoto.js

--- src/components/ProfilePhoto.js
+++ src/components/ProfilePhoto.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import l1 from '../photos/sami.jpg'
 import './ProfilePhoto.css';
+// Define the ProfilePhoto component
 function ProfilePhoto() {
+    // Return the JSX representing the profile photo
+
   return (
     <img src={l1}>
 
